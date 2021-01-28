f1dbe556c1c0d3d809771640a1ea79491cef9c63
Frigon-e <ebfrigon@gmail.com>
2021-01-28 09:38:34 -0800

    changing from server to method

--- frontend.py
+++ frontend.py
@@ -1,77 +1,53 @@
-#!/usr/bin/python3
-import sys
 from webscraper import webscraper
-from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib import parse
-
-hostName = "0.0.0.0"
-serverPort = 8080
-
-
-class MyServer(BaseHTTPRequestHandler):
-    def do_POST(self):
-        data = self.getRequestData()
-        allData = data['ids']
-
-        ids = allData.split()
-        tester = webscraper()
-        tester.get_Data(ids)
-        colNames = tester.get_Cols()
-        allData = tester.get_Rows()
-
-        tester.to_Csv()
-
-        self.set_headers(200)
-        self.wfile.write(
-            bytes("{}".format(self.get_output(colNames, allData)), "utf-8"))
-
-    def get_output(self, cols, rows):
-        output = "<table>"
-        output = output + "<tr>"
-        for x in cols:
-            output = output + "<th>" + str(x) + "</th>"
-        output = output + "</tr>"
-        for x in rows:
-            output = output + "<tr>"
-            for y in x:
-                output = output + "<td>" + str(y) + "</td>"
-            output = output + "</tr>"
-        output = output + "</table>"
-        return output
-
-    def getRequestData(self):
-        body = self.rfile.read(int(self.headers.get('Content-Length')))
-        body = body.decode("utf-8")
-        return dict(parse.parse_qsl(body))
-
-    def getParams(self):
-        output = {}
-        queryList = parse.parse_qs(parse.urlsplit(self.path).query)
-        for key in queryList:
-            if len(queryList[key]) == 1:
-                output[key] = queryList[key][0]
-        return output
-
-    def set_headers(self, responseCode):
-        self.send_response(responseCode)
-        self.send_header("Content-type", "text/html")
-        self.send_header('Access-Control-Allow-Origin', "*")
-        self.send_header('Access-Control-Allow-Headers', "*")
-        self.end_headers()
+from urllib import parse
+from webscraper import webscraper
 
 
-if __name__ == "__main__":
+def do_POST(self):
+	data = self.getRequestData()
+	allData = data['ids']
 
-    webServer = HTTPServer((hostName, serverPort), MyServer)
+	ids = allData.split()
+	tester = webscraper()
+	tester.get_Data(ids)
+	colNames = tester.get_Cols()
+	allData = tester.get_Rows()
 
-    print("Server started http://%s:%s" % (hostName, serverPort))
+	tester.to_Csv()
 
-    try:
-        webServer.serve_forever()
-    except:
-        webServer.server_close()
-        # print("Server stopped.")
-        sys.exit()
-    webServer.server_close()
-    print("Server stopped.")
-    sys.exit()
+	self.wfile.write(
+		bytes("{}".format(self.get_output(colNames, allData)), "utf-8"))
+
+
+def get_output(self, cols, rows):
+	output = "<table>"
+	output = output + "<tr>"
+	for x in cols:
+		output = output + "<th>" + str(x) + "</th>"
+	output = output + "</tr>"
+	for x in rows:
+		output = output + "<tr>"
+		for y in x:
+			output = output + "<td>" + str(y) + "</td>"
+		output = output + "</tr>"
+	output = output + "</table>"
+	return output
+
+
+def getRequestData(self):
+	body = self.rfile.read(int(self.headers.get('Content-Length')))
+	body = body.decode("utf-8")
+	return dict(parse.parse_qsl(body))
+
+
+def getParams(self):
+	output = {}
+	queryList = parse.parse_qs(parse.urlsplit(self.path).query)
+	for key in queryList:
+		if len(queryList[key]) == 1:
+			output[key] = queryList[key][0]
+	return output
+
+
+do_POST()
