d7be8d4a74c84c9e9449a4c567e65b5adec83054
Frigon-e <ebfrigon@gmail.com>
2021-02-28 14:08:33 -0800

    change index

--- webscraper.py
+++ webscraper.py
@@ -100,12 +100,12 @@ class webscraper:
 
             columnNames = allCerts
             #rowData.insert(0, id)
-            #rowData.insert(0, name)
+            rowData.insert(0, name)
             #rowDataTime.insert(0, id)
-            #rowDataTime.insert(0, name)
+            rowDataTime.insert(0, name)
 
             #columnNames.insert(0, "LSS#")
-            #columnNames.insert(0, "Name")
+            columnNames.insert(0, "Name")
 
             if i == 0:
                 self.allStaff = pd.DataFrame([rowData], columns=list(columnNames), index=[id])
