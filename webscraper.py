59398571ebe7d4150a1d9b2fdb54dd0590141329
Frigon-e <ebfrigon@gmail.com>
2021-02-28 11:53:08 -0800

    date to keep string

--- webscraper.py
+++ webscraper.py
@@ -112,17 +112,11 @@ class webscraper:
             person = pd.DataFrame([rowData], columns=list(columnNames), index=['LSS#'])
             personDate = pd.DataFrame([rowDataTime], columns=list(columnNames), index=['LSS#'])
 
-            try:
-                self.allStaff = self.allStaff.loc[~self.allStaff.index.duplicated(keep='first')]
-                self.allStaff = self.allStaff.append(person, ignore_index=True)
-            except:
-                pass
+            self.allStaff = self.allStaff.loc[~self.allStaff.index.duplicated(keep='first')]
+            self.allStaff = self.allStaff.concat([allStaff.reset_index(drop=True), person.reset_index(drop=True)])
 
-            try:
-                self.allStaff2 = self.allStaff2.loc[~self.allStaff2.index.duplicated(keep='first')]
-                self.allStaff2 = self.allStaff2.append(personDate, ignore_index=True)
-            except:
-                pass
+            self.allStaff2 = self.allStaff2.loc[~self.allStaff2.index.duplicated(keep='first')]
+            self.allStaff2 = self.allStaff2.concat([allStaff2.reset_index(drop=True), personDate.reset_index(drop=True)])
 
         self.to_Csv()
 
